631eb575f55895f0e4bcc8d934826c8c8747a4f4
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-08-21 19:46:57 +0200

    first commit

---
+++ bin/app.dart
@@ -0,0 +1,13 @@
+class Person {
+  final name;
+  Person(this.name);
+
+  String toString() => name;
+}
+
+main() {
+  var person = new Person("Frank");
+  print("Hello $person");
+  print("Hello " + person.toString());
+  print("Hello " + person);
+}
